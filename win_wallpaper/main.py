38481158694b0bba4082d8bff582c98fcbab29f8
valleyofdoom <173058957+valleyofdoom@users.noreply.github.com>
2025-03-27 21:55:52 +0000

    Update formatting

--- win_wallpaper/main.py
+++ win_wallpaper/main.py
@@ -86,9 +86,7 @@ def parse_args() -> argparse.Namespace:
 
 
 def main() -> int:
-    logging.basicConfig(
-        format="[%(name)s] %(levelname)s: %(message)s", level=logging.INFO
-    )
+    logging.basicConfig(format="[%(name)s] %(levelname)s: %(message)s", level=logging.INFO)
 
     images: set[str] = set()
 
